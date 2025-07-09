ed4d0a22ddb5dfe0c406d06b74bd0373d2cb8517
valerialainez <valeria.lainez2@ujcv.edu.hn>
2025-07-09 01:03:32 -0600

    Add files via upload

---
+++ PI_2025_II_IIP_LABORATORIO2/PI_2025_II_IIP_LABORATORIO2/Dominio/Ordenes.cs
@@ -0,0 +1,55 @@
+﻿
+using PI_2025_II_IIP_LABORATORIO2.objetos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace PI_2025_II_IIP_LABORATORIO2.objetos
+{
+    public class OrdenTrabajo
+    {
+        public string NumeroOrden {  get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaFinalización { get; set; }
+        public Clientes Clientes { get; set; }
+        public Carros Carros { get; set; }
+        public Empleado EmpleadoAsignado { get; set; }
+        public List  <Servicios> Servicios { get; set; }
+        public string Estado { get; set; } 
+        public string Observaciones { get; set; }
+
+        public OrdenTrabajo()
+        {
+            Servicios = new List<Servicios>();
+            FechaCreacion = DateTime.Now;
+            Estado = "Pendiente";
+        }
+
+        public void MostrarOrden()
+        {
+            WriteLine($"\n ORDEN DE TRABAJO #{NumeroOrden}");
+            WriteLine($"{"Fecha de Creación:",-28} {FechaCreacion}");
+            WriteLine($"{"Estado:",-28} {Estado}");
+            WriteLine($"{"Cliente:",-28} {Clientes.NombreCompleto}");
+            WriteLine($"{"Carro:",-28} {Carros.Modelo}, {Carros.Marca},{Carros.Color}, con placa: {Carros.Placa} )");
+            WriteLine($"{"Empleado Asignado:",-28} {EmpleadoAsignado.Nombre}");
+            WriteLine($"{"Servicios:",-28} {Servicios.Count} servicios asignados");
+            WriteLine ($"{"Costo Total:",-28} {Servicios.Sum(s => s.CostoServicio):C}");
+
+            WriteLine("\nServicios:");
+            foreach (var servicio in Servicios)
+            {
+                WriteLine($"- {servicio.TipoServicio} ({servicio.CostoServicio:C})");
+            }
+
+
+        }
+
+        
+
+
+    }
+}
---
+++ PI_2025_II_IIP_LABORATORIO2/PI_2025_II_IIP_LABORATORIO2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("PI_2025_II_IIP_LABORATORIO2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PI_2025_II_IIP_LABORATORIO2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("3a9cf371-096b-49b6-8fa3-24a8257643fb")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
